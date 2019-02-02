8d943c86fed0d96bf16ee9970041eb2a52519eb6
NawaF11Prince <44409762+NawaF11Prince@users.noreply.github.com>
2019-02-02 20:59:09 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '.'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Majed.`,)
+client.user.setGame(`MineCraft.`,)
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
